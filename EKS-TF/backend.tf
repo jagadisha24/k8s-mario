4b179e35f53732d0f971db09384d5ff9c2e6bd41
jagadisha24 <158666563+jagadisha24@users.noreply.github.com>
2024-03-15 07:55:38 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "ajay-mrcloudbook777" # Replace with your actual S3 bucket name
+    bucket = "jagas3awsbucket" 
     key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    region = "Asia Pacific(Mumbai) ap-south-1"
   }
 }
